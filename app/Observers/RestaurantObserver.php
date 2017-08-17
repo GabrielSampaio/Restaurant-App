da8fcdda80850c2ada8fd9a4c24efb40cc2d36b7
Gabriel Sampaio <gabriel.amorim@internit.com.br>
2017-08-17 16:22:18 -0300

    Upload de Arquivos

---
+++ app/Observers/RestaurantObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+/**
+ *
+ */
+class RestaurantObserver
+{
+  use UploadObserverTrait;
+
+  protected $field = 'photo';
+  protected $path = 'restaurante/';
+
+
+  public function creating (Restaurant $model)
+  {
+    $this->sendFile($model);
+  }
+
+  public function deleting (Restaurant $model)
+  {
+    $this->removeFile($model);
+  }
+
+  public function updating (Restaurant $model)
+  {
+    $this->updateFile($model);
+  }
+
+}

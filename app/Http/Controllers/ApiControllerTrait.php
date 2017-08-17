1bf55689fdd6f7601949d1959519d33b6e1ff1f7
Gabriel Sampaio <gabriel.amorim@internit.com.br>
2017-08-17 13:49:11 -0300

    Arquivos iniciais Lumen

---
+++ app/Http/Controllers/ApiControllerTrait.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+trait ApiControllerTrait
+{
+    public function index()
+    {
+        $results = $this->model->paginate();
+        return response()->json($results);
+    }
+}
